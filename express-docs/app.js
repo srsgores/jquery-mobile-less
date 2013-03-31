d982c11fb56f72e0ae8496bc55c2bb8deabfcf45
Sean Goresht <seangores@gmail.com>
2013-03-31 16:17:27 -0600

    Add more docs

--- express-docs/app.js
+++ express-docs/app.js
@@ -49,12 +49,66 @@ app.get("/widgets/buttons", function(req, res) {
 		title: "Buttons", description: "Description"
 	});
 });
+app.get("/widgets/dialog", function(req, res) {
+	res.render("dialog", {
+		title: "dialog", description: "Description"
+	});
+});
+
+app.get("/widgets/accordion", function(req, res) {
+	res.render("accordion", {
+		title: "accordion", description: "Description"
+	});
+});
+
+app.get("/widgets/autocomplete", function(req, res) {
+	res.render("autocomplete", {
+		title: "autocomplete", description: "Description"
+	});
+});
+
+app.get("/widgets/checkbox", function(req, res) {
+	res.render("checkbox", {
+		title: "checkbox", description: "Description"
+	});
+});
+
+app.get("/widgets/collapsible", function(req, res) {
+	res.render("collapsible", {
+		title: "collapsible", description: "Description"
+	});
+});
+
+app.get("/widgets/control-group", function(req, res) {
+	res.render("control-group", {
+		title: "control-group", description: "Description"
+	});
+});
 
 app.get("/widgets/listviews", function(req, res) {
 	res.render("listviews", {
 		title: "List Views", description: "Description"
 	});
 });
+
+app.get("/widgets/transitions", function(req, res) {
+	res.render("transitions", {
+		title: "transitions", description: "Description"
+	});
+});
+
+app.get("/widgets/page-transitions-dialog", function(req, res) {
+	res.render("page-transitions-dialog", {
+		title: "List Views", description: "Description"
+	});
+});
+
+app.get("/widgets/page-transitions-page", function(req, res) {
+	res.render("page-transitions-page", {
+		title: "List Views", description: "Description"
+	});
+});
+
 http.createServer(app).listen(app.get("port"), function ()
 {
 	console.log("Express server listening on port " + app.get("port"));
